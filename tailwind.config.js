d4b94ebef973be0597ebeff2c6162a679a85f504
vadimrubinov <59785431+vadimrubinov@users.noreply.github.com>
2026-01-25 00:16:46 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,6 @@ module.exports = {
     extend: {
       colors: {
         border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
@@ -32,11 +30,6 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
